58de5b2ca21f52c34f963961eb9d77d5c5dd2e09
kasiaurangabad <kasi.aurangabad@gmail.com>
2025-05-29 10:33:49 +0530

    v0.0.6

--- src/utils/validation/tests/stringIsFloat.test.ts
+++ src/utils/validation/tests/stringIsFloat.test.ts
@@ -72,9 +72,9 @@ describe('stringIsFloat tests', () => {
   });
 
   it('allows large numbers', () => {
-    expect(stringIsFloat('0.0000000011231723871623178236182376123')).toBe(true);
+    expect(stringIsFloat('0.0000000011231723871623178236182376123')).toBe(false);
   });
-
+  
   it('denies numbers with single comma', () => {
     expect(stringIsFloat('0,0000000011231723871623178236182376123')).toBe(
       false
