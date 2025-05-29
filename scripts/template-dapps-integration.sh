57b49c5186816e1678836ca91895fc351d5e572a
kasiaurangabad <kasi.aurangabad@gmail.com>
2025-05-29 08:54:37 +0530

    v0.0.5

--- scripts/template-dapps-integration.sh
+++ scripts/template-dapps-integration.sh
@@ -7,17 +7,19 @@ set -e
 echo "Installing yarn..."
 npm install --global yarn
 echo "Installing yalc..."
-npm install -global yalc
-
+npm install --global yalc
 
 # Prepare drt-sdk-dapp for publishing
+echo "Cloning drt-sdk-dapp..."
 git clone https://github.com/TerraDharitri/drt-sdk-dapp.git
 
 echo "cd drt-sdk-dapp..."
 cd drt-sdk-dapp
-git checkout development
 
-echo "Installing dependencies for "
+echo "Checking out main branch..."
+git checkout main
+
+echo "Installing dependencies for drt-sdk-dapp..."
 yarn install
 
 echo "Building drt-sdk-dapp..."
@@ -28,14 +30,14 @@ cd dist
 yalc publish
 cd ../..
 
-
 # Consume drt-sdk-dapp in drt-template-dapp
+echo "Cloning drt-sdk-template-dapp..."
 git clone https://github.com/TerraDharitri/drt-sdk-template-dapp.git
 
 echo "cd drt-template-dapp..."
-cd drt-template-dapp
+cd drt-sdk-template-dapp
 
-echo "Installing dependencies drt-template-dapp..."
+echo "Installing dependencies for drt-template-dapp..."
 yarn install
 
 echo "Linking drt-sdk-dapp..."
@@ -44,14 +46,16 @@ yalc add @terradharitri/sdk-dapp
 echo "Building drt-template-dapp..."
 yarn build:devnet
 
+cd ..
 
 # Consume drt-sdk-dapp in drt-template-dapp-nextjs
+echo "Cloning drt-sdk-template-dapp-nextjs..."
 git clone https://github.com/TerraDharitri/drt-sdk-template-dapp-nextjs.git
 
 echo "cd drt-template-dapp-nextjs..."
-cd drt-template-dapp-nextjs
+cd drt-sdk-template-dapp-nextjs
 
-echo "Installing dependencies drt-template-dapp-nextjs..."
+echo "Installing dependencies for drt-template-dapp-nextjs..."
 yarn install
 
 echo "Linking drt-sdk-dapp..."
